1c15e82280732dfbf9a752a98c65f2b41bdf63b4
PatrickPutz <Putz.Patrick@outlook.com>
2021-12-17 20:33:18 +0100

    factorial recursive

--- src/main/java/org/campus02/factorial/Factorial.java
+++ src/main/java/org/campus02/factorial/Factorial.java
@@ -8,6 +8,9 @@ public class Factorial {
      * @return Produkt der Multiplikationen
      */
     public int fact(int n) {
-        return 0;
+        if (n == 1)
+            return n;
+
+        return n * fact(n - 1);
     }
 }
