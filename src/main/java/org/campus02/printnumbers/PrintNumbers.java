3327b325f280f16e8b43ba3269cc0148a65c754b
PatrickPutz <Putz.Patrick@outlook.com>
2021-12-17 19:00:36 +0100

    PrintNumbers RECURSIVE - fromNToZero/fromZeroToN

--- src/main/java/org/campus02/printnumbers/PrintNumbers.java
+++ src/main/java/org/campus02/printnumbers/PrintNumbers.java
@@ -4,9 +4,37 @@ public class PrintNumbers {
 
     public static void main(String[] args) {
         fromNToZero(5);
+        System.out.println("");
+        fromNToZeroRecursive(5);
+        System.out.println("");
+        fromZeroToNRecursive(5);
     }
 
+    // iterativer Ansatz
     public static void fromNToZero(int n) {
+        while(n >= 0){
+            System.out.println("n = " + n);
+            n--;
+        }
+
+    }
+
+    // rekursiver Ansatz
+    public static void fromNToZeroRecursive(int n){
+        // Zuerst: Abbruchbestimmung
+        if(n < 0)
+            return;
+
+        System.out.println("n = " + n);
+        fromNToZeroRecursive(n - 1); // Tail Recursion -> Fußgesteuerte Rekursion
+    }
+
+    public static void fromZeroToNRecursive(int n){
+        if (n < 0)
+            return;
+
+        fromZeroToNRecursive(n - 1); // Head Recursion -> Kopfgesteuerte Rekursion
         System.out.println("n = " + n);
     }
+
 }
